6848736b5fb7f5ec0fde1336bf4b5859f0a6669d
jcaceres <jcaceres@3532cb72-8e7e-11dd-98a5-cbe9a756ba3b>
2007-10-05 16:01:04 +0000

    qwt dependency removed and working

--- jacktrip/src/MainDialog.h
+++ jacktrip/src/MainDialog.h
@@ -6,21 +6,21 @@
 
 #ifndef _MAINDIALOG_H_
 #define _MAINDIALOG_H_
-//#include "../src/ui/MainWindow.h" //Removed for compatibility with qwt-5 (we are not really using it right now)
+#include "../src/ui/MainWindow.h"
 #include "qptrlist.h"
 #include "qthread.h"
 #include "qstring.h"
 #include "qtimer.h"
 #include "StreamBD.h"
-//#include "StripChart.h" //Removed for compatibility with qwt-5 (we are not really using it right now)
+//#include "StripChart.h"
 	
-class MainDialog//:public MainWindow //Removed for compatibility with qwt-5 (we are not really using it right now)
+class MainDialog:public MainWindow
 {
       Q_OBJECT public:
 	  MainDialog (QWidget * parent = 0, const char *name = 0);
 	  virtual ~ MainDialog ();
 	  void init(StreamBD *s);
-  //StripChart *plot; //Removed for compatibility with qwt-5 (we are not really using it right now)
+  //StripChart *plot;
 	virtual void closeEvent (QCloseEvent * e);
 	void customEvent (QCustomEvent * e);
 	void addThread (QObject * t);
