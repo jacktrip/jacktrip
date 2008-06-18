55e6cde7eba6fc0e4eedfc326a04f71161b7a0d2
jcaceres <jcaceres@3532cb72-8e7e-11dd-98a5-cbe9a756ba3b>
2008-06-18 23:05:01 +0000

    AudioDevice cleanup

--- trunk/jacktrip/src/AudioDevice.cpp
+++ trunk/jacktrip/src/AudioDevice.cpp
@@ -43,12 +43,6 @@ AudioDevice::AudioDevice (bool testMode)
 	endl;
       exit (1);
     }
-  /*
-    else
-    {
-    probeAudioDevices ();
-    }
-  */
 }
 
 AudioDevice::AudioDevice (int ID, int numBuffers, audioDeviceModeT mode,
@@ -70,14 +64,10 @@ AudioDevice::AudioDevice (int ID, int numBuffers, audioDeviceModeT mode,
       harp = false;
     }
 
-  if (jack)
+  if (jack) {
     cout << "Creating Jack audio Device.........";
-  /* //Remove STK Dependency
-     else
-     cout << "Creating RtAudio Device.........";
-  */
-  //try
-  //{
+  }
+  
   switch (mode)
     {
     case RECORD:
@@ -90,21 +80,6 @@ AudioDevice::AudioDevice (int ID, int numBuffers, audioDeviceModeT mode,
 			  getFramesPerBuffer (),
 			  false, true, this, 
 			  audioInfo->getJack_alsa_readable_offset ());
-      /*//Remove STK Dependency
-	else
-	device = new RtAudio (streamID,
-	0,
-	ID,
-	audioInfo->
-	getNumAudioChans (),
-	RTAUDIO_SINT16,
-	audioInfo->
-	getSampleRate (),
-	&(tmpBufferSize),
-	numBuffers,
-	RtAudio::LINUX_ALSA);
-      */
-
       break;
     case PLAYBACK:
       if (jack)
@@ -116,18 +91,6 @@ AudioDevice::AudioDevice (int ID, int numBuffers, audioDeviceModeT mode,
 			  getFramesPerBuffer (),
 			  true, false, this, 
 			  audioInfo->getJack_alsa_readable_offset ());
-      /*//Remove STK Dependency
-	else
-	device = new RtAudio (streamID,
-	audioInfo->
-	getNumAudioChans (), ID,
-	0, RTAUDIO_SINT16,
-	audioInfo->
-	getSampleRate (),
-	&(tmpBufferSize),
-	numBuffers,
-	RtAudio::LINUX_ALSA);
-      */
       break;
     case DUPLEX:
       if (jack)
@@ -139,61 +102,11 @@ AudioDevice::AudioDevice (int ID, int numBuffers, audioDeviceModeT mode,
 			  getFramesPerBuffer (),
 			  true, true, this, 
 			  audioInfo->getJack_alsa_readable_offset ());
-      /*//Remove STK Dependency
-	else
-	device = new RtAudio ((streamID),
-	audioInfo->
-	getNumAudioChans (), ID,
-	audioInfo->
-	getNumAudioChans (),
-	RTAUDIO_SINT16,
-	audioInfo->
-	getSampleRate (),
-	&(tmpBufferSize),
-	numBuffers,
-	RtAudio::LINUX_ALSA);
-      */
       break;
     }
-  //}
-  /*//Remove STK Dependency
-    catch (RtError (&m))
-    {
-    cout << "FAILED!" << endl;
-    m.printMessage ();
-    probeAudioDevices ();
-    exit (1);
-    }
-  */
+  
   cout << "SUCCESS!" << endl;
-
-  /*//Remove STK Dependency
-    if (tmpBufferSize != audioInfo->getFramesPerBuffer ())
-    {
-    cerr << "RtAudio device could not use requested buffer length of " << audioInfo->getFramesPerBuffer () << " frames.  Using " << tmpBufferSize << endl;
-    audioInfo->setFramesPerBuffer (tmpBufferSize);
-    }
-  */
-
-  /*//Remove STK Dependency
-    if (!jack)
-    {
-    if (device == NULL)
-    {
-    cerr << "RtAudio Device is NULL!" << endl;
-    probeAudioDevices ();
-    }
-    buffer = device->getStreamBuffer ();
-
-
-    if (buffer == NULL)
-    {
-    cerr << "RtAudio device buffer is NULL!" << endl;
-    probeAudioDevices ();
-    }
-    }
-  */
-
+  
   bytesPerBuffer = audioInfo->getBytesPerBuffer ();
 
   if (harp == true)
@@ -211,8 +124,6 @@ AudioDevice::AudioDevice (int ID, int numBuffers, audioDeviceModeT mode,
   if (jack)
     {
     }
-  //else //Remove STK Dependency
-  // device->startStream ();
 }
 
 AudioDevice::~AudioDevice ()
@@ -233,46 +144,6 @@ AudioDevice::jackStop ()
   jdevice->stop ();
 }
 
-/*
-  void
-  AudioDevice::tick ()
-  {
-  try
-  {
-  device->tickStream (); // blocks until transfer completed
-  if (audioInfo->lastTickTime)
-  {
-  clock_t newTime = clock ();
-  fprintf (stderr, "Audio Tick,%d,%d,%.5f\n", newTime,
-  *(audioInfo->lastTickTime),
-  (newTime -
-  *(audioInfo->lastTickTime)) *
-  CLOCKS_PER_SEC);
-  *(audioInfo->lastTickTime) = newTime;
-  }
-  }
-  catch (RtError (&m))
-  {
-  m.printMessage ();
-  }
-  }
-*/
-
-void
-AudioDevice::jtick () // called by jack process after transfer completed
-{
-  //	jackWait.wakeAll ();
-  //      cout << "jtick" << endl;
-}
-
-/*
-  void
-  AudioDevice::bufferPtrs (void *jib, void *job)
-  {
-  //	jibuffer = jib;
-  //	jobuffer = job;
-  }
-*/
 
 void
 AudioDevice::unlockRead ()
@@ -316,52 +187,13 @@ AudioDevice::writeBuffer (void *from)
       (*readLock).release();//****JPC qt4 porting******
 		
     }
-  else
-    {
-		
-      harpTick (buffer, from);
-      //tick ();
-		
-    }
 }
 
-void
-AudioDevice::harpTick (void *toBuffer, void *fromBuffer)
-{
-  double nextSample;
-  int stringIndex = 0;
-  int sndChans = audioInfo->getNumAudioChans ();
-  int netChans = audioInfo->getNumNetHarpStrings ();
-  for (int frameIndex = 0;
-       frameIndex < audioInfo->getFramesPerBuffer (); frameIndex++)
-    {
-      stringIndex = 0;
-      for (int chanIndex = 0; chanIndex < sndChans; chanIndex++)
-	{
-	  nextSample = 0.0;
-	  for (int sampleIndex = 0;
-	       sampleIndex < stringsPerChan[chanIndex];
-	       sampleIndex++)
-	    {
-	      nextSample +=
-		(double) (*
-			  ((signed short
-			    *) (fromBuffer) +
-			   frameIndex *
-			   netChans +
-			   stringIndex++)) / 32768.0;
-	    }
-	  *((signed short *) (toBuffer) +
-	    frameIndex * sndChans + chanIndex) =
-	    (signed short) floor (nextSample * 32768.0);
-	}
-    }
-}
+
 
 /**
  * @brief Distribute the NetHarp channels across the audio channels.
  */
-
 void
 AudioDevice::generateHarpMixMap ()
 {
@@ -387,78 +219,6 @@ AudioDevice::generateHarpMixMap ()
     }
 }
 
-
-/**
- * @brief Probe audio device to see if it supports required functionality.
- */
-/*//Remove STK Dependency
-  void
-  AudioDevice::probeAudioDevices ()
-  {
-  cout << "=====================================================" <<
-  endl;
-  cout << " Probing audio devices..." << endl;
-  cout << " ------------------------" << endl << endl;
-
-  try
-  {
-  device = new RtAudio ();
-  } catch (RtError (&m))
-  {
-  cerr << "AudioDevice::probeAudioDevice() failed!" << endl;
-  m.printMessage ();
-  exit (1);
-  }
-  int numDevices = device->getDeviceCount ();
-
-  RtAudioDeviceInfo info;
-
-  cout << " " << numDevices << " audio devices found.  Probing..." <<
-  endl << endl;
-
-  for (int i = 0; i < numDevices; i++)
-  {
-  try
-  {
-  info = device->getDeviceInfo (i + 1);
-  checkRequirements (i + 1, &info);
-  }
-  catch (RtError & error)
-  {
-  error.printMessage ();
-  break;
-  }
-  }
-  cout << "Check these values against the streambd default command line settings." << endl;
-  cout << "Change the relevant parameters as required.  If duplex is not supported" << endl;
-  cout << "you will only be able to run in SNDMIRROR mode." << endl;
-  cout << "=====================================================" <<
-  endl;
-  }
-*/
-
-/*//Remove STK Dependency
-  void
-  AudioDevice::checkRequirements (int deviceNum, RtAudioDeviceInfo * info)
-  {
-  cout << "Device " << deviceNum << ": " << info->name << endl;
-  cout << "maxOutputChannels =\t" << info->outputChannels << endl;
-  cout << "maxInputChannels =\t" << info->inputChannels << endl;
-  cout << "maxDuplexChannels =\t" << info->duplexChannels << endl;
-
-  cout << "hasDuplexSupport =\t";
-  if (info->duplexChannels > 0)
-  cout << "true" << endl;
-  else
-  cout << "false." << endl;
-  cout << "Sample rates supported: " << endl;
-  for (int i = 0; i < 99; i++)
-  if ((info->
-  sampleRates[i] > 0) && (info->sampleRates[i] < 200000))
-  cout << info->sampleRates[i] << endl;
-  cout << endl;
-  }
-*/
 
 void
 AudioDevice::setThreads (streamThreads t)
