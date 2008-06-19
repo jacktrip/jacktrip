0b0cb3abd8e4f02409c6089ac637e39ac7254217
jcaceres <jcaceres@3532cb72-8e7e-11dd-98a5-cbe9a756ba3b>
2008-06-19 17:54:10 +0000

    added audiointerface.h file

---
+++ jacktrip/src2/AudioInterface.h
@@ -0,0 +1,30 @@
+/*
+ * AudioInterface.h
+ * author: Juan-Pablo Caceres
+ * date: June 2008
+ */
+
+
+#ifndef __AUDIOINTERFACE_H__
+#define __AUDIOINTERFACE_H__
+
+
+/** \brief Abstract class the provides an audio interface
+ *
+ */
+class AudioInterface
+{
+public:
+  AudioInterface();
+  virtual ~AudioInterface();
+
+  virtual void start() = 0;
+  virtual void stop() = 0;
+
+protected:
+  int mNumInChans;///< Number of Input Channels
+  int mNumOutChans; ///<  Number of Output Channels
+  int mNumFrames; ///< Buffer block size, in samples
+};
+
+#endif
