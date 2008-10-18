7aea2aa2eadc48428f7d93d39aa233dcce086d2d
jcacerec <jcacerec@3532cb72-8e7e-11dd-98a5-cbe9a756ba3b>
2008-10-18 01:10:38 +0000

    Exceptions added, and make worker thread exception "safe"

--- jacktrip/src/JackTripWorker.cpp
+++ jacktrip/src/JackTripWorker.cpp
@@ -65,42 +65,58 @@ void JackTripWorker::setJackTrip(int id, uint32_t client_address,
 //*******************************************************************************
 void JackTripWorker::run()
 {
-  JackTrip* jacktrip = new JackTrip(JackTrip::CLIENT,
-				    JackTrip::UDP,
-				    mNumChans,2);
-
-  jacktrip->setPeerAddress( mClientAddress.toString().toLatin1().data() );
-  jacktrip->setLocalPorts(mServerPort);
-  jacktrip->setPeerPorts(mClientPort-1);
-
-  //NetKS* netks = new NetKS;
-  //jacktrip->appendProcessPlugin(mNetks);
-  //mNetks->play();
-
-  cout << "AFTER PLAYING" << endl;
-
-  //QTimer *timer = new QTimer(this);
-  //QObject::connect(timer, SIGNAL(timeout()), netks, SLOT(exciteString()));
-  //timer->start(300);
-  //QObject::connect(timer, SIGNAL(timeout()), netks, SLOT(exciteString()));
-  //timer->start(300);
-
- 
   /*
-  LoopBack* loopback = new LoopBack(mNumChans);
-  jacktrip->appendProcessPlugin(loopback);
+    NOTE: This is the message that qt prints when an exception is thrown:
+    'Qt Concurrent has caught an exception thrown from a worker thread.
+    This is not supported, exceptions thrown in worker threads must be
+    caught before control returns to Qt Concurrent.'
   */
-  jacktrip->start();
   
-  mSpawning = false;
-  cout << "================BEFORE BLOCK EXEC===========" << endl;
-  exec();
-  exit();
-  cout << "================BLOCK EXEC===========" << endl;
-
-  jacktrip->wait(); // wait for jacktrip to be done
-  cout << "AFTER WAIT(((((((((((((((((((((((" << endl;
-
+  // Try catching any exceptions that come from JackTrip
+  try 
+    {
+      JackTrip* jacktrip = new JackTrip(JackTrip::CLIENT,
+					JackTrip::UDP,
+					mNumChans,2);
+      
+      jacktrip->setPeerAddress( mClientAddress.toString().toLatin1().data() );
+      jacktrip->setLocalPorts(mServerPort);
+      jacktrip->setPeerPorts(mClientPort-1);
+      
+      //NetKS* netks = new NetKS;
+      //jacktrip->appendProcessPlugin(mNetks);
+      //mNetks->play();
+      
+      cout << "AFTER PLAYING" << endl;
+      
+      //QTimer *timer = new QTimer(this);
+      //QObject::connect(timer, SIGNAL(timeout()), netks, SLOT(exciteString()));
+      //timer->start(300);
+      //QObject::connect(timer, SIGNAL(timeout()), netks, SLOT(exciteString()));
+      //timer->start(300);
+      
+      
+      /*
+	LoopBack* loopback = new LoopBack(mNumChans);
+	jacktrip->appendProcessPlugin(loopback);
+      */
+      jacktrip->start();
+      
+      mSpawning = false;
+      cout << "================BEFORE BLOCK EXEC===========" << endl;
+      exec();
+      exit();
+      cout << "================BLOCK EXEC===========" << endl;
+      
+      jacktrip->wait(); // wait for jacktrip to be done
+      cout << "AFTER WAIT(((((((((((((((((((((((" << endl;
+    }
+  catch ( const std::exception & e )
+    {
+      std::cerr << e.what() << endl;
+      std::cerr << "Exiting Program..." << endl;
+      std::cerr << gPrintSeparator << endl;
+    }
   /*
   QObject::connect(jacktrip, SIGNAL(JackTripStopped()),
 		   udpmasterlistener, SLOT(setValue(int)));
