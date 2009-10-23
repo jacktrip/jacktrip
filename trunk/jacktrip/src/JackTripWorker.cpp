ab04a495388f03c7e84cb68888d03d72f519d70c
jcacerec <jcacerec@3532cb72-8e7e-11dd-98a5-cbe9a756ba3b>
2009-10-23 20:39:40 +0000

    fixed bug in worker class

--- trunk/jacktrip/src/JackTripWorker.cpp
+++ trunk/jacktrip/src/JackTripWorker.cpp
@@ -231,12 +231,14 @@ int JackTripWorker::setJackTripFromClientHeader(JackTrip& jacktrip)
       //cout << "---------> ELAPSED TIME: " << elapsedTime << endl;
     }
   }
+  /*
   sleep.wait(&mutex,100);
   if (!UdpSockTemp.hasPendingDatagrams()) {
     std::cerr << "--->JackTripWorker: is not receiving Datagrams" << endl;
     UdpSockTemp.close();
     return -1;
   }
+  */
   int packet_size = UdpSockTemp.pendingDatagramSize();
   char packet[packet_size];
   UdpSockTemp.readDatagram(packet, packet_size);
