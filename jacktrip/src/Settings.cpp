5d3a2c3197cb21861f92f5e4567f355c2f9838b2
jua20133 <jua20133@adobe.com>
2015-05-26 20:34:04 -0300

    updated copyright year

--- jacktrip/src/Settings.cpp
+++ jacktrip/src/Settings.cpp
@@ -282,7 +282,7 @@ void Settings::printUsage()
     cout << "" << endl;
     cout << "JackTrip: A System for High-Quality Audio Network Performance" << endl;
     cout << "over the Internet" << endl;
-    cout << "Copyright (c) 2008-2009 Juan-Pablo Caceres, Chris Chafe." << endl;
+    cout << "Copyright (c) 2008-2015 Juan-Pablo Caceres, Chris Chafe." << endl;
     cout << "SoundWIRE group at CCRMA, Stanford University" << endl;
     cout << "VERSION: " << gVersion << endl;
     cout << "-----------------------------------------------------------------------------" << endl;
