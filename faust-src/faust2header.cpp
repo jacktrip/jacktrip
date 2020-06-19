fee8a039b2d8eaadd5e28ebe8429ef535c073e4a
Julius Smith <julius.smith@gmail.com>
2020-06-19 00:33:03 -0700

    work around faust -i bug

--- faust-src/faust2header.cpp
+++ faust-src/faust2header.cpp
@@ -5,11 +5,9 @@
 // aimed at creating a simple C++ header file (.h) containing a Faust DSP.
 // See the Makefile for how to use it.
 
-#include <faust/gui/APIUI.h>
-
-//?using namespace std;
-
 #include <faust/dsp/dsp.h>
+
+#include <faust/gui/APIUI.h>
 
 // NOTE: "faust -scn name" changes the last line above to
 // #include <faust/name/name.h>
