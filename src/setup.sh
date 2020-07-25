b89d368bf16ec2bf81cad33ea025805091c815fa
Julius Smith <julius.smith@gmail.com>
2020-07-25 16:24:35 -0700

    delete temp files

--- src/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -fx
-# set up for debugging:
-#jackd -T -ndefault -dcoreaudio -r48000 -p1024 &
-/usr/local/bin/jackdmp -T -ndefault -dcoreaudio -r48000 -p1024 &
-sleep 2
-./jacktrip -V -s -p4 &
-echo 'in debugger, set args to <whatever you want plus> -c 127.0.0.1 -o10'
-ps aux | grep jack
--- src/unsetup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -fx
-# unsetup for debugging:
-ps aux | grep jack
-killall jackdmp
-killall jacktrip
-ps aux | grep jack
-echo 'debugger jacktrip must be stopped manually'
