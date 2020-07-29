a883191329ace138796eff24c88a0f2e74165373
Aaron Wyatt <github@psi-borg.org>
2020-07-29 13:24:50 +1000

    QJackTrip - JackTrip with a GUI
    I thought I might as well get this out there so that people can at least play around with it and we can iron out the details further down the line. I'd really like to release the GUI part of this under the GPL, so that's what I've done here. (Since it'll be easier for me to re-release the code under a more permissive license later if need be rather than trying to impose a copyleft license after the horse has already bolted.)
    
    Building should have the same requirements as the command line version. There is the option to use CMake, but just running "./build-script" should do the job and output the executable to the build folder. Additionally, in the "os x" folder, there's a script to put together an application bundle. You'll need to make sure the qt bin folder is in your PATH environment variable for it to work.

---
+++ qjacktrip.pro
@@ -0,0 +1,177 @@
+#******************************
+# Created by Juan-Pablo Caceres
+#******************************
+
+CONFIG += c++11 console
+CONFIG -= app_bundle
+
+CONFIG += qt thread debug_and_release build_all
+CONFIG(debug, debug|release) {
+  TARGET = qjacktrip_debug
+  } else {
+  TARGET = qjacktrip
+  }
+
+QT += gui
+QT += network
+QT += widgets
+
+# rc.1.2 switch enables experimental wair build, merge some of it with WAIRTOHUB
+# DEFINES += WAIR
+DEFINES += WAIRTOHUB
+
+# http://wiki.qtcentre.org/index.php?title=Undocumented_qmake#Custom_tools
+#cc DEFINES += __RT_AUDIO__
+# Configuration without Jack
+nojack {
+  DEFINES += __NO_JACK__
+}
+
+# for plugins
+INCLUDEPATH += faust-src-lair/stk
+
+!win32 {
+  INCLUDEPATH+=/usr/local/include
+# wair needs stk, can be had from linux this way
+# INCLUDEPATH+=/usr/include/stk
+# LIBS += -L/usr/local/lib -ljack -lstk -lm
+  LIBS += -L/usr/local/lib -ljack -lm
+  nojack {
+    message(Building NONJACK)
+    LIBS -= -ljack
+  }
+}
+
+macx {
+  message(Building on MAC OS X)
+  QMAKE_CXXFLAGS += -D__MACOSX_CORE__ #-D__UNIX_JACK__ #RtAudio Flags
+  QMAKE_MACOSX_DEPLOYMENT_TARGET = 10.9
+  #QMAKE_MAC_SDK = macosx10.9
+  CONFIG -= app_bundle
+  #CONFIG += x86 #ppc #### If you have both libraries installed, you
+  # can change between 32bits (x86) or 64bits(x86_64) Change this to go back to 32 bits (x86)
+  LIBS += -framework CoreAudio -framework CoreFoundation
+  DEFINES += __MAC_OSX__
+  }
+
+linux-g++ | linux-g++-64 {
+#   LIBS += -lasound -lrtaudio
+  QMAKE_CXXFLAGS += -D__LINUX_ALSA__ #-D__LINUX_OSS__ #RtAudio Flags
+
+FEDORA = $$system(cat /proc/version | grep -o fc)
+
+contains( FEDORA, fc): {
+  message(building on fedora)
+}
+
+UBUNTU = $$system(cat /proc/version | grep -o Ubuntu)
+
+contains( UBUNTU, Ubuntu): {
+  message(building on  Ubuntu)
+
+# workaround for Qt bug under ubuntu 18.04
+# gcc version 7.3.0 (Ubuntu 7.3.0-16ubuntu3)
+# QMake version 3.1
+# Using Qt version 5.9.5 in /usr/lib/x86_64-linux-gnu
+  INCLUDEPATH += /usr/include/x86_64-linux-gnu/c++/7
+
+# sets differences from original fedora version
+  DEFINES += __UBUNTU__
+}
+
+  QMAKE_CXXFLAGS += -g -O2
+  DEFINES += __LINUX__
+  }
+
+linux-g++ {
+  message(Linux)
+  QMAKE_CXXFLAGS += -D__LINUX_ALSA__ #-D__LINUX_OSS__ #RtAudio Flags
+  }
+
+linux-g++-64 {
+  message(Linux 64bit)
+  QMAKE_CXXFLAGS += -fPIC -D__LINUX_ALSA__ #-D__LINUX_OSS__ #RtAudio Flags
+  }
+
+
+win32 {
+  message(Building on win32)
+#cc  CONFIG += x86 console
+  CONFIG += c++11 console
+  INCLUDEPATH += "C:\Program Files (x86)\Jack\includes"
+  LIBS += "C:\Program Files (x86)\Jack\lib\libjack64.lib"
+  LIBS += "C:\Program Files (x86)\Jack\lib\libjackserver64.lib"
+#cc  QMAKE_CXXFLAGS += -D__WINDOWS_ASIO__ #-D__UNIX_JACK__ #RtAudio Flags
+  #QMAKE_LFLAGS += -static -static-libgcc -static-libstdc++ -lpthread
+  LIBS += -lWs2_32 #cc -lOle32 #needed by rtaudio/asio
+  DEFINES += __WIN_32__
+  DEFINES += _WIN32_WINNT=0x0600 #needed for inet_pton
+#cc    DEFINES -= UNICODE #RtAudio for Qt
+}
+
+DESTDIR = .
+QMAKE_CLEAN += -r ./jacktrip ./jacktrip_debug ./release ./debug
+target.path = /usr/bin
+INSTALLS += target
+
+# for plugins
+INCLUDEPATH += faust-src-lair
+
+# Input
+HEADERS += src/DataProtocol.h \
+           src/JMess.h \
+           src/JackTrip.h \
+           src/jacktrip_globals.h \
+           src/jacktrip_types.h \
+           src/JackTripWorker.h \
+           src/LoopBack.h \
+           src/PacketHeader.h \
+           src/ProcessPlugin.h \
+           src/RingBuffer.h \
+           src/RingBufferWavetable.h \
+           src/Settings.h \
+           src/UdpDataProtocol.h \
+           src/UdpHubListener.h \
+           src/AudioInterface.h \
+           src/about.h \
+           src/qjacktrip.h
+#(Removed JackTripThread.h JackTripWorkerMessages.h NetKS.h TestRingBuffer.h ThreadPoolTest.h)
+
+!nojack {
+HEADERS += src/JackAudioInterface.h
+}
+SOURCES += src/DataProtocol.cpp \
+           src/JMess.cpp \
+           src/JackTrip.cpp \
+           src/jacktrip_globals.cpp \
+           src/JackTripWorker.cpp \
+           src/LoopBack.cpp \
+           src/PacketHeader.cpp \
+           src/RingBuffer.cpp \
+           src/Settings.cpp \
+           src/UdpDataProtocol.cpp \
+           src/UdpHubListener.cpp \
+           src/AudioInterface.cpp \
+	   src/about.cpp \
+           src/main.cpp \
+           src/qjacktrip.cpp
+#(Removed jacktrip_main.cpp jacktrip_tests.cpp JackTripThread.cpp ProcessPlugin.cpp)
+
+!nojack {
+SOURCES += src/JackAudioInterface.cpp
+}
+
+FORMS += src/qjacktrip.ui src/about.ui
+RESOURCES += src/qjacktrip.qrc
+
+# RtAduio Input
+win32 {
+  INCLUDEPATH += externals/rtaudio-4.1.1/include
+  DEPENDPATH += externals/rtaudio-4.1.1/include
+}
+macx | win32 {
+INCLUDEPATH += externals/rtaudio-4.1.1/
+DEPENDPATH += externals/rtaudio-4.1.1/
+HEADERS +=
+SOURCES +=
+}
---
+++ src/main.cpp
@@ -0,0 +1,154 @@
+//*****************************************************************
+/*
+  QJackTrip: Bringing a graphical user interface to JackTrip, a
+  system for high quality audio network performance over the
+  internet.
+
+  Copyright (c) 2020 Aaron Wyatt.
+  
+  This file is part of QJackTrip.
+
+  QJackTrip is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  QJackTrip is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+*/
+//*****************************************************************
+
+#include "qjacktrip.h"
+#include <QApplication>
+#include <QScopedPointer>
+#include <iostream>
+#include <signal.h>
+#include "jacktrip_globals.h"
+#include "Settings.h"
+#include "UdpHubListener.h"
+#include <QLoggingCategory>
+
+QCoreApplication *createApplication(int &argc, char *argv[])
+{
+    if (argc == 1) {
+        return new QApplication(argc, argv);
+    } else {
+        return new QCoreApplication(argc, argv);
+    }
+}
+
+void qtMessageHandler(__attribute__((unused)) QtMsgType type, __attribute__((unused)) const QMessageLogContext &context, const QString &msg)
+{
+    std::cerr << msg.toStdString() << std::endl;
+}
+
+#if defined (__LINUX__) || (__MAC_OSX__)
+static int setupUnixSignalHandler(void (*handler)(int))
+{
+    //Setup our SIGINT handler.
+    struct sigaction sigInt;
+    sigInt.sa_handler = handler;
+    sigemptyset(&sigInt.sa_mask);
+    sigInt.sa_flags = 0;
+    sigInt.sa_flags |= SA_RESTART;
+    
+    if (sigaction(SIGINT, &sigInt, 0)) {
+        return 1;
+    }
+    return 0;
+}
+#else
+bool isHubServer = false;
+
+BOOL WINAPI windowsCtrlHandler(DWORD fdwCtrlType)
+{
+    switch (fdwCtrlType) {
+        case CTRL_C_EVENT:
+            if (isHubServer) {
+                UdpHubListener::sigIntHandler(0);
+            } else {
+                JackTrip::sigIntHandler(0);
+            }
+            return true;
+        default:
+            return false;
+    }
+}
+#endif
+
+int main(int argc, char *argv[])
+{
+    QScopedPointer<QCoreApplication> app(createApplication(argc, argv));
+    QScopedPointer<QJackTrip> w;
+    QScopedPointer<JackTrip> jackTrip;
+    QScopedPointer<UdpHubListener> udpHub;
+    
+    if (qobject_cast<QApplication *>(app.data())) {
+        //Start the GUI if there are no command line options.
+#ifdef __WIN_32__
+        //Remove the console that appears if we're in windows.
+        FreeConsole();
+#endif
+        w.reset(new QJackTrip);
+        QObject::connect(w.data(), &QJackTrip::signalExit, app.data(), &QCoreApplication::quit, Qt::QueuedConnection);
+        w->show();
+    } else {
+        //Otherwise use the non-GUI version, and parse our command line.
+        QLoggingCategory::setFilterRules(QStringLiteral("*.debug=true"));
+        qInstallMessageHandler(qtMessageHandler);
+        try {
+            Settings settings;
+            settings.parseInput(argc, argv);
+            
+            //Either start our hub server or our jacktrip process as appropriate.
+            if (settings.isHubServer()) {
+                udpHub.reset(settings.getConfiguredHubServer());
+                if (gVerboseFlag) std::cout << "Settings:startJackTrip before udphub->start" << std::endl;
+                QObject::connect(udpHub.data(), &UdpHubListener::signalStopped, app.data(),
+                                 &QCoreApplication::quit, Qt::QueuedConnection);
+                QObject::connect(udpHub.data(), &UdpHubListener::signalError, app.data(),
+                                 &QCoreApplication::quit, Qt::QueuedConnection);
+#if defined (__LINUX__) || (__MAC_OSX__)
+                setupUnixSignalHandler(UdpHubListener::sigIntHandler);
+#else
+                isHubServer = true;
+                SetConsoleCtrlHandler(windowsCtrlHandler, true);
+#endif
+                udpHub->start();
+            } else {
+                jackTrip.reset(settings.getConfiguredJackTrip());
+                if (gVerboseFlag) std::cout << "Settings:startJackTrip before mJackTrip->startProcess" << std::endl;
+                QObject::connect(jackTrip.data(), &JackTrip::signalProcessesStopped, app.data(),
+                                 &QCoreApplication::quit, Qt::QueuedConnection);
+                QObject::connect(jackTrip.data(), &JackTrip::signalError, app.data(),
+                                 &QCoreApplication::quit, Qt::QueuedConnection);
+#if defined (__LINUX__) || (__MAC_OSX__)
+                setupUnixSignalHandler(JackTrip::sigIntHandler);
+#else
+                std::cout << SetConsoleCtrlHandler(windowsCtrlHandler, true) << std::endl;
+#endif
+#ifdef WAIRTOHUB // WAIR
+                jackTrip->startProcess(0); // for WAIR compatibility, ID in jack client name
+#else
+                jackTrip->startProcess();
+#endif // endwhere
+            }
+            
+            if (gVerboseFlag) std::cout << "step 6" << std::endl;
+            if (gVerboseFlag) std::cout << "jmain before app->exec()" << std::endl;
+        } catch (const std::exception &e) {
+            std::cerr << "ERROR:" << std::endl;
+            std::cerr << e.what() << std::endl;
+            std::cerr << "Exiting JackTrip..." << std::endl;
+            std::cerr << gPrintSeparator << std::endl;
+            return -1;
+        }
+    }
+    
+    return app->exec();
+}
