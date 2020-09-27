204b27a1de6fcb6382b9208fe974619a0f639c28
Julius Smith <julius.smith@gmail.com>
2020-09-27 13:12:06 -0700

    postpone alterations of the -b 24 case until it can be fixed and tested in both directions on both big and little endian platforms

--- src/AudioInterface.cpp
+++ src/AudioInterface.cpp
@@ -488,16 +488,6 @@ void AudioInterface::computeProcessToNetwork(QVarLengthArray<sample_t*>& in_buff
     mJackTrip->sendNetworkPacket( mInputPacket );
 } // /computeProcessToNetwork
 
-bool is_big_endian(void)
-{
-    union {
-        uint32_t i;
-        char c[4];
-    } bint = {0x01020304};
-
-    return bint.c[0] == 1;  // 1 if bigEndine, 4 if littleEndian
-}
-
 //*******************************************************************************
 // This function quantize from 32 bit to a lower bit resolution
 // 24 bit is not working yet
@@ -507,8 +497,11 @@ void AudioInterface::fromSampleToBitConversion
  const AudioInterface::audioBitResolutionT targetBitResolution)
 {
     int8_t tmp_8;
+    uint8_t tmp_u8; // unsigned to quantize the remainder in 24bits
     int16_t tmp_16;
     double tmp_sample;
+    sample_t tmp_sample16;
+    sample_t tmp_sample8;
     switch (targetBitResolution)
     {
     case BIT8 :
@@ -524,17 +517,23 @@ void AudioInterface::fromSampleToBitConversion
         tmp_16 = static_cast<int16_t>(tmp_sample);
         std::memcpy(output, &tmp_16, 2); // 2 bytes output in Little Endian order (LSB -> smallest address)
         break;
-    case BIT24 : {
-        // tmp_sample = (*input) * 32768.0; // 2^15 = 32768.0
-        tmp_sample = std::max(-8388607.0, std::min(8388607.0, std::round( (*input) * 8388607.0 ))); // 2^23 = 8388608
-        union {
-          uint32_t tmp32;
-          char tmp32c[4];
-        } tmp32u;
-        tmp32u.tmp32 = tmp_sample;
-        int offset = ( is_big_endian() ? 1 : 0 );
-        std::memcpy(output, &(tmp32u.tmp32c[offset]), 3); // 24bits = 3 bytes
-        break; }
+   case BIT24 :
+        // To convert to 24 bits, we first quantize the number to 16bit
+        tmp_sample = (*input) * 32768.0; // 2^15 = 32768.0
+        tmp_sample16 = floor(tmp_sample);
+        tmp_16 = static_cast<int16_t>(tmp_sample16);
+
+        // Then we compute the remainder error, and quantize that part into an 8bit number
+        // Note that this remainder is always positive, so we use an unsigned integer
+        tmp_sample8 = floor ((tmp_sample - tmp_sample16)  //this is a positive number, between 0.0-1.0
+                             * 256.0);
+        tmp_u8 = static_cast<uint8_t>(tmp_sample8);
+
+        // Finally, we copy the 16bit number in the first 2 bytes,
+        // and the 8bit number in the third bite
+        std::memcpy(output, &tmp_16, 2); // 16bits = 2 bytes
+        std::memcpy(output+2, &tmp_u8, 1); // 8bits = 1 bytes
+        break;
     case BIT32 :
         tmp_sample = *input;
         // not necessary yet:
