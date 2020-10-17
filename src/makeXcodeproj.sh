cf8b19ba1d2b7acf5d2426557c1d937910429e65
Julius Smith <julius.smith@gmail.com>
2020-10-16 20:57:07 -0700

    add meson case to src/makeXcodeproj.sh

--- src/makeXcodeproj.sh
+++ src/makeXcodeproj.sh
@@ -1,1 +1,3 @@
-qmake -spec macx-xcode jacktrip.pro
+echo "qmake -spec macx-xcode jacktrip.pro"
+      qmake -spec macx-xcode jacktrip.pro
+echo "For meson version: cd .. && meson setup builddir --backend xcode"
