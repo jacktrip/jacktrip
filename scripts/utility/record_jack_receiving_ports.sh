cc2d823b47f8fb0f047f6cc240e6a14eb5b7f29d
wholenotes <tersewings@protonmail.com>
2020-11-14 16:13:00 -0800

    ecasound script now records up to 4 receive and broadcast channels

--- scripts/utility/record_jack_receiving_ports.sh
+++ scripts/utility/record_jack_receiving_ports.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
-
+#use this script to record jacktrip receive & broadcast channels on ecasound
+#all clients must be connected to server before running this script
 if ! pgrep -x "jackd" ; then
   printf "Please start jack with a 48K sample rate before running this script."
   exit
 fi
 
-IPs=($(jack_lsp | grep ":receive_*" | cut -d: -f1))
-uniqIPs=($(printf "%s\n" "${IPs[@]}" | sort -u | tr '\n' ' '))
+IPs=($(jack_lsp | grep ':receive_*\|:broadcast_*' | cut -d: -f1))
 
-uniqLength=${#uniqIPs[@]}
-printf '%s\n' "number of unique IP's: ${uniqLength}"
-
-for IP in "${uniqIPs[@]}"
+for IP in "${IPs[@]}"
 do
   printf '%s\n' "IP: ${IP}"
   NOW=$( date '+%F_%H:%M:%S' )
-  ecasound -f:,1,48000 -i jack,${IP} -o rec_${IP}_${NOW}_output.wav &
+  ecasound -f:,4,48000 -i jack,${IP} -o rec_${IP}_${NOW}_output.wav &
 done 
 
 echo "Press ESC key to stop recording"
