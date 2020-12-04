f8c0bd054eab34af67ac1361591d8e1a9e5e70b0
Aaron Wyatt <github@psi-borg.org>
2020-12-04 15:56:55 +1100

    Fix infinite loop in UdpHubListener
    Fix a case where an infinite loop can occur when trying to remove a previous JackTripWorker thread for a given client.

--- src/JackTripWorker.h
+++ src/JackTripWorker.h
@@ -110,6 +110,8 @@ public:
     void setIOStatTimeout(int timeout) { mIOStatTimeout = timeout; }
     void setIOStatStream(QSharedPointer<std::ofstream> statStream) { mIOStatStream = statStream; }
     
+    QString getAssignedClientName() { return mAssignedClientName; }
+    
 private slots:
     void slotTest()
     { std::cout << "--- JackTripWorker TEST SLOT ---" << std::endl; }
@@ -134,10 +136,12 @@ private:
     int mBufferQueueLength;
     JackTrip::underrunModeT mUnderRunMode;
     QString mClientName;
+    QString mAssignedClientName;
 
     /// Thread spawning internal lock.
     /// If true, the prototype is working on creating (spawning) a new thread
     volatile bool mSpawning;
+    bool mStopped;
     QMutex mMutex; ///< Mutex to protect mSpawning
 
     int mID; ///< ID thread number
