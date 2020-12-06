a4709665f9e647763762cda5e5bc0acd0235dbc7
Julius Smith <julius.smith@gmail.com>
2020-12-05 23:00:25 -0800

    whitespace

--- src/tmulaw.cpp
+++ src/tmulaw.cpp
@@ -12,7 +12,7 @@ int main() {
     short mih = MuLawToLinearSample(mlmi);
     char mlistr[5]; sprintf(mlistr, "0x%02x",mli);
     char mlmistr[5]; sprintf(mlmistr, "0x%02x",mlmi);
-    
+
     std::cout << "mulaw[" << i << "] == " << mlistr << " -> " << ih << "\t|\tmulaw[" << -i << "] == " << mlmistr << " -> " << mih << "\n";
   }
 
