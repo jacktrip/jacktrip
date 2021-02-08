849d988a1aa875c494c6fed60920f29e6a5876b2
Chris Chafe <4406287+cchafe@users.noreply.github.com>
2021-02-08 11:41:16 -0800

    test mReceiverPeerPort in JackTrip.cpp

--- src/JackTrip.cpp
+++ src/JackTrip.cpp
@@ -186,6 +186,11 @@ void JackTrip::setupAudio(
             && (mJackClientName.constData() == gJackDefaultClientName.constData())) {
 //            mJackClientName = QString(mPeerAddress).replace(":", "_");
             mJackClientName = QString(mPeerAddress).replace(":", "_") + QString("srcUDP") + QString::number(mReceiverPeerPort);
+qDebug() << " " << mReceiverBindPort << mSenderPeerPort
+        << mSenderBindPort
+        << mReceiverPeerPort ;
+
+
         }
         //std::cout  << "WAIR ID " << ID << " jacktrip client name set to=" <<
         //              mJackClientName.toStdString() << std::endl;
