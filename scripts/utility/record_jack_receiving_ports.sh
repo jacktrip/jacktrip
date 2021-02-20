1b5d2d28fc621995a17310b72bba66c214b902c8
wholenotes <tersewings@protonmail.com>
2021-02-20 00:48:02 -0800

    changed script to record broadcast channels too

--- scripts/utility/record_jack_receiving_ports.sh
+++ scripts/utility/record_jack_receiving_ports.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
+#use this script to record jacktrip receive & broadcast channels on ecasound
+#Dependency: Ecasound https://ecasound.seul.org/ecasound/README
+#Make sure you have ecasound installed in the recording environment (server or client)
+#Start JACK and JackTrip on the server or the client you want to record from
+#IMPORTANT: Wait till all clients have connected
+#cd to the folder where this script is located
+#Start the script from the terminal: ./record_jack_receiving_ports.sh
+#Press ESC to stop recording.
+#Recording should appear as a .wav file in the same folder as the script
 
 if ! pgrep -x "jackd" ; then
   printf "Please start jack with a 48K sample rate before running this script."
   exit
 fi
 
-IPs=($(jack_lsp | grep ":receive_*" | cut -d: -f1))
-uniqIPs=($(printf "%s\n" "${IPs[@]}" | sort -u | tr '\n' ' '))
+IPs=($(jack_lsp | grep ':receive_*\|:broadcast_*' | cut -d: -f1))
 
-uniqLength=${#uniqIPs[@]}
-printf '%s\n' "number of unique IP's: ${uniqLength}"
-
-for IP in "${uniqIPs[@]}"
+for IP in "${IPs[@]}"
 do
   printf '%s\n' "IP: ${IP}"
   NOW=$( date '+%F_%H:%M:%S' )
-  ecasound -f:,1,48000 -i jack,${IP} -o rec_${IP}_${NOW}_output.wav &
+  ecasound -f:,4,48000 -i jack,${IP} -o rec_${IP}_${NOW}_output.wav &
 done 
 
 echo "Press ESC key to stop recording"
