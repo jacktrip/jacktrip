d6631e76ad3c264ea657c284d284b28c07172264
cc <xxx>
2021-05-21 12:51:47 -0700

    revised comments

--- scripts/hubMode/test_hub_mode_server_and_clientPipeWire.sh
+++ scripts/hubMode/test_hub_mode_server_and_clientPipeWire.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 # test_hub_mode_server_and_clientPipeWire.sh <JACKTRIP> <FPP>
+
 # bash script for automatic testing of jacktrip in hub mode , Chris Chafe
 # connects a hub client to a hub server (started with with -p1) on the same host
-# requires jack_iodelay and jmess systemwide
-
 # prints avg audio RTT after 8 sec, or -1 if fail 
-# # requires 3 helper scripts
+
+# requires jack_iodelay be installed and available from system
+
+# # requires 3 helper scripts in the same directory
 # -- startJacktripHubServerPipeWire.sh
 # -- startJacktripHubClientPipeWire.sh
 # -- artPipeWire.sh
 
-# takes approx. 20 sec to complete
-# a passing test prints to the console
+# example session:
 
 # [cc@localhost sh]$ ./test_hub_mode_server_and_clientPipeWire.sh /home/cc/jacktrip/builddir/jacktrip 32
 # starting /home/cc/jacktrip/builddir/jacktrip hub mode test at 32 FPP
@@ -20,16 +21,6 @@
 # 4
 
 # a failed test prints to the console
-# [cc@localhost functionTests]$ ./test.sh ~/jacktrip/builddir/jacktrip 32
-# starting /home/cc/jacktrip/builddir/jacktrip hub mode test at 32 FPP
-# starting /home/cc/jacktrip/builddir/jacktrip server
-# starting hub client of server running on localhost
-# calculate audio round trip
-# ERROR ..1:send_1 not a valid port
-# ERROR ..1:send_2 not a valid port
-# ERROR ..1:send_1 not a valid port
-# ERROR ..1:receive_1 not a valid port
-# jacktrip: no process found
 # -1
 
 JACKTRIP=$1
