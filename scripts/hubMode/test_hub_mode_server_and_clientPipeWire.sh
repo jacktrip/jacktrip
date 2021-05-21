ff302affb696494312cbc25dff69a14aec1f6e39
cc <xxx>
2021-05-21 13:13:07 -0700

    new subdir for old examples, option for auto jackd start for CI

--- scripts/hubMode/test_hub_mode_server_and_clientPipeWire.sh
+++ scripts/hubMode/test_hub_mode_server_and_clientPipeWire.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-# test_hub_mode_server_and_clientPipeWire.sh <JACKTRIP> <FPP>
+# test_hub_mode_server_and_clientPipeWire.sh <JACKTRIP> <FPP> <JACKD>
 
-# bash script for automatic testing of jacktrip in hub mode , Chris Chafe
+# bash script for automatic testing of jacktrip in hub mode, Chris Chafe
 # connects a hub client to a hub server (started with with -p1) on the same host
+
+# to run jackd automatically, set JACKD to anything (e.g., 1)
+#    for example, for CI testing
+# prints avg audio RTT after about 20 sec, or -1 if fail 
+
+# to not run jackd automatically, leave off the 3rd argument
+#     for example, if starting jackd manually with qjackctl or using PipeWire
 # prints avg audio RTT after 8 sec, or -1 if fail 
 
 # requires jack_iodelay be installed and available from system
@@ -26,7 +33,32 @@
 JACKTRIP=$1
 FPP=$2
 
-echo starting $JACKTRIP hub mode test at $FPP FPP
+if [ -z "$3" ]
+  then
+    JACKD=0
+  else
+    JACKD=$3
+fi
+
+echo starting $JACKTRIP hub mode test at $FPP FPP with JACKD = $JACKD
+
+if [ $JACKD != 0 ]
+  then
+    echo starting JACKD
+# killall jackd
+    if [ "$(ps -aux | grep -c jackd)" != 1 ]; then killall jackd; fi;
+# if jackd is or has been running with another driver
+# much experimenation shows it literally takes this long
+    sleep 17
+# to flush old connections before starting the dummy driver
+
+# start jack with dummy driver, or change to an audio interface by switching these lines
+    $( /usr/bin/jackd  -ddummy -r48000 -p$FPP > /dev/null 2>&1 & )
+# $( /usr/bin/jackd  -dalsa -dhw:A96 -r48000 -p$FPP -n2  > /dev/null 2>&1 & )
+# $( /usr/bin/jackd  -dalsa -dhw:PCH -r48000 -p$FPP -n2  > /dev/null 2>&1 & )
+
+    sleep 1
+fi
 
 $PWD/startJacktripHubServerPipeWire.sh $JACKTRIP $FPP
 sleep 1
