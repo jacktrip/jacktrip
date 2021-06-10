8e46aba5318085a624309ed6b9964c71b5ede7f9
Aaron Wyatt <github@psi-borg.org>
2021-06-10 19:02:07 +1000

    Use wix preprocessor
    Turns out that wix has a preprocessor. Use this to simplify installer creation. (Can substitute version info into the installer more easily, and don't need a separate files_static.wxs)

--- win/build_installer.bat
+++ win/build_installer.bat
@@ -15,6 +15,7 @@ if not defined WIXPATH (
 	set WIXPATH=C:\Program Files ^(x86^)\!WIXPATH!\bin
 )
 if not defined SSLPATH (set SSLPATH=C:\Qt\Tools\OpenSSL\Win_x64\bin)
+if not defined RTAUDIOLIB (set RTAUDIOLIB="C:\Program Files (x86)\RtAudio\bin\librtaudio.dll")
 
 set PATH=%PATH%;%WIXPATH%
 del deploy /s /q
@@ -25,31 +26,35 @@ copy license.rtf deploy\
 if exist ..\builddir\release\jacktrip.exe (set JACKTRIP=..\builddir\release\jacktrip.exe) else (set JACKTRIP=..\builddir\jacktrip.exe)
 copy %JACKTRIP% deploy\
 cd deploy
-set WIXFILES=files_static
+set "WIXDEFINES="
 for /f "tokens=*" %%a in ('%QTLIBPATH%\objdump -p jacktrip.exe ^| findstr Qt5Core.dll') do set DYNAMIC_QT=%%a
 if defined DYNAMIC_QT (
+	echo Including Qt Files
 	%QTBINPATH%\windeployqt jacktrip.exe
 	copy "%QTLIBPATH%\libgcc_s_seh-1.dll" .\
 	copy "%QTLIBPATH%\libstdc++-6.dll" .\
 	copy "%QTLIBPATH%\libwinpthread-1.dll" .\
 	copy "%SSLPATH%\libcrypto-1_1-x64.dll" .\
 	copy "%SSLPATH%\libssl-1_1-x64.dll" .\
-	set WIXFILES=files
+	set WIXDEFINES=%WIXDEFINES% -ddynamic
 )
-copy ..\%WIXFILES%.wxs .\
+for /f "tokens=*" %%a in ('%QTLIBPATH%\objdump -p jacktrip.exe ^| findstr librtaudio.dll') do set RTAUDIO=%%a
+if defined RTAUDIO (
+	echo Including librtaudio
+	copy %RTAUDIOLIB% .\
+	set WIXDEFINES=%WIXDEFINES% -drtaudio
+)
+
+copy ..\jacktrip.wxs .\
+copy ..\files.wxs .\
 .\jacktrip --test-gui
 if %ERRORLEVEL% NEQ 0 (
-	echo "You need to build jacktrip with gui support to build the installer."
+	echo You need to build jacktrip with gui support to build the installer.
 	exit /b 1
 )
 rem Get our version number
 for /f "tokens=*" %%a in ('.\jacktrip -v ^| findstr VERSION') do for %%b in (%%~a) do set VERSION=%%b
 echo Version=%VERSION%
-for /f "tokens=* delims=" %%a in (..\jacktrip.wxs.template) do (
-	set line=%%a
-	set line=!line:$VERSION=%VERSION%!
-	echo !line! >> jacktrip.wxs
-)
-candle.exe jacktrip.wxs %WIXFILES%.wxs
-light.exe -ext WixUIExtension -o JackTrip.msi jacktrip.wixobj %WIXFILES%.wixobj
+candle.exe -dVersion=%VERSION%%WIXDEFINES% jacktrip.wxs files.wxs
+light.exe -ext WixUIExtension -o JackTrip.msi jacktrip.wixobj files.wixobj
 endlocal
