465efad38f127b423ae1fc000bbaf095637cc502
Aaron Wyatt <github@psi-borg.org>
2022-04-22 12:41:15 +1000

    Clang format fix

--- src/gui/virtualstudio.cpp
+++ src/gui/virtualstudio.cpp
@@ -129,7 +129,9 @@ void VirtualStudio::show()
         std::cout << "SSL Library: " << sslVersion.toStdString() << std::endl;
         if (sslVersion.isEmpty()) {
             QMessageBox msgBox;
-            msgBox.setText(QStringLiteral("OpenSSL was not found. You will not be able to connect to the Virtual Studio server."));
+            msgBox.setText(
+                QStringLiteral("OpenSSL was not found. You will not be able to connect "
+                               "to the Virtual Studio server."));
             msgBox.setWindowTitle(QStringLiteral("SSL Error"));
             msgBox.exec();
         }
