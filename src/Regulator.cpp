97fdd55d40fa0a01f4623772cba0142528663800
Chris Chafe <cchafe@github.com>
2022-05-19 13:37:39 -0700

    fix #538 crash on D_GLIBCXX_ASSERTIONS) because of array bounds, Regulator.cpp line 423 needs to be i < (cd->trainSamps - 2)

--- src/Regulator.cpp
+++ src/Regulator.cpp
@@ -420,7 +420,7 @@ void Regulator::processChannel(int ch, bool glitch, int packetCnt, bool lastWasG
 
             ba.predict(cd->mCoeffs, cd->mTail);  // resizes to TRAINSAMPS-2 + TRAINSAMPS
 
-            for (int i = 0; i < (cd->trainSamps - 1); i++)
+            for (int i = 0; i < (cd->trainSamps - 2); i++)
                 cd->mPrediction[i] = cd->mTail[i + cd->trainSamps];
         }
         // cross fade last prediction with mTruth
