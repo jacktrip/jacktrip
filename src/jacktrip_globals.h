38db0be8b249145a65b48fe8a7363a2d3dfa8e1e
Chris Chafe <cchafe@github.com>
2022-05-31 10:25:39 -0700

    bump version to release 1.6.0

--- src/jacktrip_globals.h
+++ src/jacktrip_globals.h
@@ -40,7 +40,7 @@
 
 #include "AudioInterface.h"
 
-constexpr const char* const gVersion = "1.6.0-rc.5";  ///< JackTrip version
+constexpr const char* const gVersion = "1.6.0";  ///< JackTrip version
 
 //*******************************************************************************
 /// \name Default Values
