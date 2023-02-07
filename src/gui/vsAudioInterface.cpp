885b8be3d7c14fa9201104dd6ecfe79cc4d9761d
Dominick Hing <dominick.hing1024@gmail.com>
2023-02-06 16:24:25 -0800

    fix mono for channel 1

--- src/gui/vsAudioInterface.cpp
+++ src/gui/vsAudioInterface.cpp
@@ -211,7 +211,7 @@ void VsAudioInterface::setupRtAudio()
 #ifdef RT_AUDIO
     if constexpr (isBackendAvailable<AudioInterfaceMode::ALL>()
                   || isBackendAvailable<AudioInterfaceMode::RTAUDIO>()) {
-        m_audioInterface.reset(new RtAudioInterface(m_numAudioChansIn, m_numAudioChansOut,
+        m_audioInterface.reset(new RtAudioInterface(m_baseInputChannel, m_numAudioChansIn, m_numAudioChansOut,
                                                     m_audioBitResolution));
         m_audioInterface->setSampleRate(m_sampleRate);
         m_audioInterface->setDeviceID(m_deviceID);
@@ -272,8 +272,6 @@ void VsAudioInterface::closeAudio()
             emit errorToProcess(QString::fromUtf8(e.what()));
         }
         m_audioInterface.clear();
-        m_numAudioChansIn  = gDefaultNumInChannels;
-        m_numAudioChansOut = gDefaultNumOutChannels;
         m_deviceID         = gDefaultDeviceID;
     }
 }
