46a35551ee02f2ba1fda87fa2deebce9115366eb
Dominick Hing <dominick.hing1024@gmail.com>
2023-03-14 16:51:50 -0700

    remove prompt=login

--- src/gui/virtualstudio.cpp
+++ src/gui/virtualstudio.cpp
@@ -1013,7 +1013,6 @@ void VirtualStudio::toVirtualStudio()
                 (*parameters)[QStringLiteral("code")] = QUrl::fromPercentEncoding(code);
             } else if (stage == QAbstractOAuth2::Stage::RequestingAuthorization) {
                 parameters->insert(QStringLiteral("audience"), AUTH_AUDIENCE);
-                parameters->insert(QStringLiteral("prompt"), QStringLiteral("login"));
             }
             if (!parameters->contains("client_id")) {
                 parameters->insert("client_id", AUTH_CLIENT_ID);
@@ -2040,7 +2039,6 @@ void VirtualStudio::setupAuthenticator()
             } else if (stage == QAbstractOAuth2::Stage::RequestingAuthorization) {
                 parameters->insert(QStringLiteral("audience"),
                                    QStringLiteral("https://api.jacktrip.org"));
-                parameters->insert(QStringLiteral("prompt"), QStringLiteral("login"));
             }
         });
 
