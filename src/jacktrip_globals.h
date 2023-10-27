550bda0088fff8edc41f97211b5ffce94c8aaa4a
Mike Dickey <mike@mikedickey.com>
2023-10-26 19:16:20 -0700

    Version number bump to 2.0.1-beta1 and changelog updates

--- src/jacktrip_globals.h
+++ src/jacktrip_globals.h
@@ -40,7 +40,7 @@
 
 #include "AudioInterface.h"
 
-constexpr const char* const gVersion = "2.1.0";  ///< JackTrip version
+constexpr const char* const gVersion = "2.1.0-beta1";  ///< JackTrip version
 
 //*******************************************************************************
 /// \name Default Values
