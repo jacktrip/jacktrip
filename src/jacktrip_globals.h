0391f753ae1b26358baa4e50d62de56f6acf4556
Mike Dickey <mike@mikedickey.com>
2024-01-16 10:45:06 -0800

    Flatpak test: Just changing the version string, no beta

--- src/jacktrip_globals.h
+++ src/jacktrip_globals.h
@@ -40,7 +40,7 @@
 
 #include "AudioInterface.h"
 
-constexpr const char* const gVersion = "2.1.1";  ///< JackTrip version
+constexpr const char* const gVersion = "2.2.0";  ///< JackTrip version
 
 //*******************************************************************************
 /// \name Default Values
