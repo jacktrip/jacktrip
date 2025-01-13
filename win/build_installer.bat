a89bd25f13de1ec68f91362d69bb02c177c88f52
Mike Dickey <mike@mikedickey.com>
2025-01-12 23:52:28 -0800

    Updates to build and install VST3 plugin for Windows
    Bumping version to 2.5.0-beta1 and updated changelog

--- win/build_installer.bat
+++ win/build_installer.bat
@@ -38,6 +38,16 @@ if "%~1"=="/q" (
 )
 if exist ..\builddir\release\jacktrip.exe (set JACKTRIP=..\builddir\release\jacktrip.exe) else (set JACKTRIP=..\builddir\jacktrip.exe)
 copy %JACKTRIP% deploy\
+if exist ..\builddir\JackTrip.vst3 (
+    echo Including JackTrip.vst3
+	mkdir deploy\JackTrip.vst3
+	mkdir deploy\JackTrip.vst3\Contents
+	xcopy /E ..\src\vst3\resources deploy\JackTrip.vst3\Contents\Resources\
+	copy ..\LICENSE.md deploy\JackTrip.vst3\Contents\Resources\LICENSE.md
+	xcopy /E ..\LICENSES deploy\JackTrip.vst3\Contents\Resources\LICENSES\
+	mkdir deploy\JackTrip.vst3\Contents\x86_64-win
+	copy ..\builddir\JackTrip.vst3 deploy\JackTrip.vst3\Contents\x86_64-win\JackTrip.vst3
+)
 cd deploy
 
 set "WIXDEFINES="
@@ -65,17 +75,23 @@ if defined DYNAMIC_QT (
 	set WIXDEFINES=!WIXDEFINES! -ddynamic -dqt%QTVERSION%
 )
 
-copy ..\jacktrip.wxs .\
-copy ..\qt%QTVERSION%.wxs .\
 .\jacktrip --test-gui
 if %ERRORLEVEL% NEQ 0 (
 	echo You need to build jacktrip with gui support to build the installer.
 	exit /b 1
 )
+
 rem Get our version number
 for /f "tokens=*" %%a in ('.\jacktrip -v ^| findstr VERSION') do for %%b in (%%~a) do set VERSION=%%b
 for /f "tokens=1 delims=-" %%a in ("%VERSION%") do set VERSION=%%a
 echo Version=%VERSION%
-candle.exe -arch x64 -ext WixUIExtension -ext WixUtilExtension -dVersion=%VERSION%%WIXDEFINES% ..\jacktrip.wxs ..\qt%QTVERSION%.wxs
-light.exe -ext WixUIExtension -ext WixUtilExtension -o JackTrip.msi jacktrip.wixobj qt%QTVERSION%.wixobj
+
+if exist JackTrip.vst3 (
+	powershell -Command "(gc JackTrip.vst3\Contents\Resources\moduleinfo.json) -replace '%%VERSION%%', '%VERSION%' | Out-File -encoding ASCII JackTrip.vst3\Contents\Resources\moduleinfo.json"
+	candle.exe -arch x64 -ext WixUIExtension -ext WixUtilExtension -dvst=true -dVersion=%VERSION%%WIXDEFINES% ..\jacktrip.wxs ..\jacktrip-vst3.wxs ..\qt%QTVERSION%.wxs
+	light.exe -ext WixUIExtension -ext WixUtilExtension -o JackTrip.msi jacktrip.wixobj jacktrip-vst3.wixobj qt%QTVERSION%.wixobj
+) else (
+	candle.exe -arch x64 -ext WixUIExtension -ext WixUtilExtension -dVersion=%VERSION%%WIXDEFINES% ..\jacktrip.wxs ..\qt%QTVERSION%.wxs
+	light.exe -ext WixUIExtension -ext WixUtilExtension -o JackTrip.msi jacktrip.wixobj qt%QTVERSION%.wixobj
+)
 endlocal
